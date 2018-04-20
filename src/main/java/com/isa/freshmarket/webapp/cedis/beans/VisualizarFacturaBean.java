5ef5560130e702d0aacbf4ddd9648cf3e39e9a3c
Emmanuel Santana <emmanuel.santana@hotmail.com>
2018-04-20 13:56:03 -0500

    Updated VisualizarFactura (added xml)

--- src/main/java/com/isa/freshmarket/webapp/cedis/beans/VisualizarFacturaBean.java
+++ src/main/java/com/isa/freshmarket/webapp/cedis/beans/VisualizarFacturaBean.java
@@ -6,6 +6,7 @@
 package com.isa.freshmarket.webapp.cedis.beans;
 
 import com.isa.facturacion.api.client.Facturas;
+import com.isa.facturacion.api.model.ComprobanteDTO;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Serializable;
@@ -69,18 +70,22 @@ public class VisualizarFacturaBean implements Serializable {
         FacesContext fc = FacesContext.getCurrentInstance();
         ExternalContext ec = fc.getExternalContext();
 
-        ec.responseReset(); // Some JSF component library or some Filter might have set some headers in the buffer beforehand. We want to get rid of them, else it may collide.
-        ec.setResponseContentType("text/plain"); // Check http://www.iana.org/assignments/media-types for all types. Use if necessary ExternalContext#getMimeType() for auto-detection based on filename.
-        ec.setResponseContentLength(5); // Set it with the file size. This header is optional. It will work if it's omitted, but the downloadPDF progress will be unknown.
-        ec.setResponseHeader("Content-Disposition", "attachment; filename=\"" + invoiceID + ".xml\""); // The Save As popup magic is done here. You can give it any file name you want, this only won't work in MSIE, it will use current request URL as file name instead.
-
-        OutputStream output = ec.getResponseOutputStream();
         // Now you can write the InputStream of the file to the above OutputStream the usual way.
-        // ...
-        String textoPrueba = "Hola!";
-        output.write(textoPrueba.getBytes());
-        output.close();
-        fc.responseComplete(); // Important! Otherwise JSF will attempt to render the response which obviously will fail since it's already written with a file and closed.
+        byte[] bytesXML = new Facturas().getXML(invoiceID);
+        if (bytesXML == null) {
+            showMessage("Atencion", "La factura que buscas, no existe", FacesMessage.SEVERITY_WARN);
+        } else {
+            ec.responseReset(); // Some JSF component library or some Filter might have set some headers in the buffer beforehand. We want to get rid of them, else it may collide.
+            ec.setResponseContentType("application/xml"); // Check http://www.iana.org/assignments/media-types for all types. Use if necessary ExternalContext#getMimeType() for auto-detection based on filename.
+            ec.setResponseContentLength(bytesXML.length); // Set it with the file size. This header is optional. It will work if it's omitted, but the downloadPDF progress will be unknown.
+            ec.setResponseHeader("Content-Disposition", "attachment; filename=\"" + invoiceID + ".xml\""); // The Save As popup magic is done here. You can give it any file name you want, this only won't work in MSIE, it will use current request URL as file name instead.
+
+            OutputStream output = ec.getResponseOutputStream();
+            output.write(bytesXML);
+            output.close();
+            fc.responseComplete(); // Important! Otherwise JSF will attempt to render the response which obviously will fail since it's already written with a file and closed.
+        }
+
     }
 
     public String getInvoiceID() {
